3c1251cc542e3246b64244aa7e3e0e1dc3aeebd6
UltraMik <eyalgo@017.net.il>
2020-06-10 11:20:56 +0300

    Updates

---
+++ precache-manifest.7bf347c8a2955ddf59b3895c4561cc8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26f81e07549d0928aad9b0547c2e24c",
+    "url": "/gol/index.html"
+  },
+  {
+    "revision": "2414407c917733aef90b",
+    "url": "/gol/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/gol/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gol/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2414407c917733aef90b",
+    "url": "/gol/static/js/main.c370ba89.chunk.js"
+  },
+  {
+    "revision": "052ff529ea7dd7f4f583",
+    "url": "/gol/static/js/runtime-main.24b02042.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gol/static/media/logo.5d5d9eef.svg"
+  }
+]);
